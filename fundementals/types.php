3c55c754c042c04e377638f752d8ac7097f6d736
megaminded <megamindsetsolutions@gmail.com>
2021-01-27 15:05:25 +0100

    Discussed about some of the php datatypes

---
+++ fundementals/types.php
@@ -0,0 +1,19 @@
+<?php
+$integer = 2021;
+$boolean = true; // false
+$string = "This is a string variable";
+$arrays = [3, "Hello", "Hi", true];
+
+$man_u = 2;
+$chelsea = 0;
+
+// echo "Chelsea: {$chelsea} : Man U: {$man_u}";
+$numerator = 10;
+$denuminator = 2;
+
+$result = $numerator / $denuminator;
+
+// echo $result;
+
+var_dump($result);
+// var_dump($arrays);
