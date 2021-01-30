65bb2ff4a4ae4d6246d2f38c5fb04d24ce8b748a
megaminded <megamindsetsolutions@gmail.com>
2021-01-30 12:20:01 +0100

    Renamed the file of the itenary class

--- fundementals/itenary_operator.php
+++ fundementals/itenary_operator.php
@@ -1,4 +1,4 @@
 <?php
 $x  = 2;
-# Is more like the ifelse condition that checks if a value is true or false;
+# Is more like the if else condition that checks if a value is true or false;
 echo $x == 2 ? 'Yes' : 'No';
