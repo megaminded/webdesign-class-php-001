c1ee14a13f86deb996b7b18a8437819db9696a16
megaminded <megamindsetsolutions@gmail.com>
2021-01-30 12:25:02 +0100

    Discussed the various ways we can cast variables in php

---
+++ fundementals/type_casting.php
@@ -0,0 +1,11 @@
+<?php
+$string = "24x4 hours";
+
+$time = (int) $string;
+// We have various data to cast variables, they include:
+# String (string)
+# Integer (int)
+# Boolean (bool)
+# Array (array)
+# Object (object)
+echo $time;
