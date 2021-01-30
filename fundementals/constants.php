3a3e9665e0fb1cdf842a5b510b5061be662b3b55
megaminded <megamindsetsolutions@gmail.com>
2021-01-30 12:14:25 +0100

    Learnt the basics of constants in php

---
+++ fundementals/constants.php
@@ -0,0 +1,6 @@
+<?php
+# Using the define function with the first parameter as the name of the constant and the second parameter as the value of the constant,
+# Values of these constants can either be strings, int, or boolean, arrays are also supported in php version >= 7.1
+define("NAME", "James");
+
+echo NAME;
