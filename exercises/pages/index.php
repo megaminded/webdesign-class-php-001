a80c0ecaff1acf2b85706f6e32f812f2558da6f5
megaminded <megamindsetsolutions@gmail.com>
2021-02-12 21:00:43 +0100

    The various approach used for seperating files using require and include, explaining the difference between both

--- exercises/pages/index.php
+++ exercises/pages/index.php
@@ -1,17 +1,5 @@
 <?php include("../inc/header.html") ?>
 
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident facere culpa omnis nihil perferendis. Consequatur velit obcaecati labore animi ducimus perspiciatis voluptas quod dignissimos ullam aliquam tempore, esse assumenda molestias?</p>
-
-<?php echo "Francis"; ?>
-
-
-<?php
-$numbers = array(19,1,24,31,12,5,6,7,8,9,24,13);
-$index = 0;
-while ($index < count($numbers)) {
-    echo $numbers[$index] . ',';
-    $index++;
-}
-?>
-
+<h1><?php echo "Index"; ?></h1>
+<a href="services.php">Our services</a>
 <?php include("../inc/footer.html") ?>
---
+++ exercises/pages/services.php
@@ -0,0 +1,11 @@
+<?php include("../inc/header.html") ?>
+<?php require("../inc/config.php") ?>
+<?php // include_once("../inc/config.php") 
+?>
+<?php // require_once("../inc/config.php") 
+?>
+
+<h1><?php echo SITE_NAME . " Services"; ?></h1>
+
+
+<?php include("../inc/footer.html") ?>
